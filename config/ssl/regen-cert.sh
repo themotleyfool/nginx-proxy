8173fe7af312d922750182d70a25854bf29ac045
Tim White <twhite@fool.com>
2021-06-08 09:40:37 -0600

    Updating certs and config.

--- config/ssl/regen-cert.sh
+++ config/ssl/regen-cert.sh
@@ -1,4 +1,4 @@
-openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout localhost.key -config <( cat server.csr.cnf )
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout localhost.key -config server.csr.cnf
 openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out localhost.crt -days 500 -sha256 -extfile v3.ext -passin pass:fool
 mv localhost.key ../nginx/
 mv localhost.crt ../nginx/
